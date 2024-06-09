0bee026dfc464bc9764badb9d1f26d3740cf9684
NatalieShif <natalia.shif@gmail.com>
2024-06-09 09:53:21 +0300

    first commit

---
+++ notes/sql.sql
@@ -0,0 +1,32 @@
+/****
+DB SERVER
+שרת של בסיס הנתונים:
+we start it on our machine with some
+DB program like MySQL Workbench or
+XAMPP+MySQL or MS SQL Server or MongoDB
+or we use some running server elsewhere
+
+DB SERVER INSTANCE - 
+במקרים רבים - שקול לשרת של בסיס הנתונים,
+כי מדובר בתהליכי 
+DB PROGRAM
+שרצים, משתמשים במעבד, זיכרון, דיסק
+
+DataBase - בסיס נתונים
+בדיבור - יכול להתייחס לשרת עצמו, אבל
+המונח המדויק מתייחס לסכמה -
+Schema
+- אוסף של טבלאות בנושא מסוים
+או טבלאות ששייכות לאפליקציה מסוימת
+או צרור של אפליקציות
+
+---------------------
+
+Our clients for MySQL are:
+   MySQL Workbench
+   phpMyAdmin (in our local machine)
+   phpMyAdmin (in cPanel of the hosting)
+   yummy.js
+   tirgol.js
+
+*/

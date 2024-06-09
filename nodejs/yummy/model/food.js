310f6925446642e5ae8479338f5aca93f4f017d6
NatalieShif <natalia.shif@gmail.com>
2024-06-09 16:57:28 +0300

    asyncronous function and callback

--- nodejs/yummy/model/food.js
+++ nodejs/yummy/model/food.js
@@ -1,10 +1,21 @@
+const cuisine = require('./cuisine');
+
 module.exports = {
 
     getAddPage: (req, res) => {
 
-        res.render('add-food.ejs', {
-            title: 'Add | Yummy',
-            message: ''
+        cuisine.getCuisineList((err,result) => {
+
+            if (err) {
+                return res.status(500).send(err.message);
+            }
+
+            res.render('add-food.ejs', {
+                title: 'Add | Yummy',
+                message: '',
+                cuisines: result
+            })
+
         })
 
     }
